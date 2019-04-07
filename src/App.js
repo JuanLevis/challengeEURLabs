079982c4562d7bcf21b34d2ff3190d11bd32d986
Juan Levis <juan.levis9425@gmail.com>
2019-04-07 20:34:04 -0300

    Conexion con api, StockCard y alert cuando el limite de peticiones se alcanzo.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import InvestorPage from './pages/InvestorPage';
 import Navbar from 'react-bootstrap/Navbar';
